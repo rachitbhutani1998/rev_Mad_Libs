0cf25836bb0fb107447d9b6e483fee95dce06c1c
rachit <rachitbhutani1998@gmail.com>
2017-08-14 23:32:52 +0530

    added comments

--- revMadLibs.py
+++ revMadLibs.py
@@ -1,3 +1,4 @@
+# 3 Questions on the basis of increasing difficulty
 que1=['''
         In Game of __1__, Tyrion is introduced as the third and youngest child
         of wealthy and powerful __2__ Lannister, the former __3__ of the King, and
@@ -25,24 +26,30 @@ que3=['''
         constant reminder of Ned's infidelity.
         ''','jon','stark','winterfell','arya','catelyn']
 
+#Following functions handles the gameplay
 def play_game(updated_que,blank_number):
+    '''The functions handle the question statement updation corresponding to
+    which blank is being answered.
+    updated_que:    question statement after every response of user.
+    blank_number:   the blank user is currently giving the response of.'''
     while blank_number<=5:
         print '\n'+str(blank_number)+'\n'
         ans=raw_input(updated_que[0])
-        if ans==updated_que[blank_number]:
-            updated_que[0]=updated_que[0].replace('__'+str(blank_number)+'__',ans)
+        if ans==updated_que[blank_number]:#this conditions check if the answer is correct
+            updated_que[0]=updated_que[0].replace('__'+str(blank_number)+'__',ans)#updating the question statement
             blank_number=blank_number+1
-            play_game(updated_que,blank_number)
+            play_game(updated_que,blank_number)#proceeding to next blank
         elif ans=='quit':
             exit()
         else:
             print '\nTry Again\n'
-            play_game(updated_que,blank_number)
+            play_game(updated_que,blank_number)#handling the wrong answer response
     print '\n'+updated_que[0]+'\nThanks for playing. You may exit by entering quit or choose another question.\n'
     start_game()
 
 
 def start_game():
+    '''The game starts here'''
     que=raw_input('\nWhich question you want to go for?\n 1, 2 or 3 for easy, medium or hard respectively?\nEnter quit to exit anytime.\n')
     if que=='1':
         play_game(que1,1)
